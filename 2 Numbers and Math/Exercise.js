4cacecb9c64c5fbb6ed595b120089324cc1849e6
Asthapuram Chanikya <asthapuramchanikya@gmail.com>
2025-03-07 14:27:06 +0530

    Add files via upload

---
+++ 2 Numbers and Math/Exercise.js
@@ -0,0 +1,47 @@
+// 2a Exercise
+let soup=10;
+let burger=8
+let icecream=5;
+soup+3*burger+icecream
+// 2b Exercise
+let asoup=10;
+let bburger=8
+let cicecream=5;
+3*(soup+3*burger+icecream)
+// 2c Exercise
+let toaster = 18.50;
+let shirt = 7.50;
+toaster+2*shirt
+// 2d Exercise
+let atoaster = 18.50;
+let bshirt = 7.50;
+(toaster+2*shirt)*(0.1)
+// 2e Exercise
+let ctoaster = 18.50;
+let dshirt = 7.50;
+(toaster+2*shirt)*(0.2)
+// 2f Exercise
+(2095+799+1899)/100
+ans = 47.93
+// 2g Exercise
+((2095+799+1899+499)*0.1)/(100)
+ans=5.292000000000001
+// 2h Exercise
+Math.round((2095+799+1899+499)*0.1)/(100)
+ans=5.29
+// 2i Exercise
+(2095+799+1899+499)/100 + Math.round((2095+799+1899+499)*0.1)/(100)
+ans = 58.21
+// 2j Exercise
+Math.floor(2.8)
+// 2k Exercise
+Math.ceil(2.2)
+// 2l Exercise
+Fahrenheit = (Celsius * 9/5) + 32
+Celsius = (Fahrenheit - 32) * 5/9
+Fahrenheit = (25 * 9/5) + 32
+//25C =  77F
+Celsius = (86 - 32) * 5/9
+//86F = 30C
+Fahrenheit = (-5 * 9/5) + 32
+//-5C = 23 F
