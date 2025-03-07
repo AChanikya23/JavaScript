03346eddb6f2504667b8f7199a2f05a1f9ed3b41
Asthapuram Chanikya <asthapuramchanikya@gmail.com>
2025-03-07 10:55:36 +0530

    Add files via upload

---
+++ 1 JavaScript Basics/Exercise.js
@@ -0,0 +1,27 @@
+// 1a Exercise
+alert('Good morning!'); 
+// 1b Exercise 
+alert('ASTHSPURAM CHANIKYA');
+// 1c Exercise
+10+5
+// 1d Exercise
+20-5
+// 1e Exercise
+2+2-5
+// 1f Exercise
+document.body.innerHTML='Good morning';
+// 1g Exercise
+document.body.innerHTML='ASTHSPURAM CHANIKYA';
+// 1h Exercise
+let tshirt=10;
+let socks=8;
+let dinnerplates=20;
+tshirt+socks+dinnerplates
+// 1i Exercise
+let acc=100;
+let spend=20;
+let lunch=50;
+let job=200;
+acc-spend-lunch+job
+// 1j Exercise
+document.body.innerHTML=' ';
