b252fc83e75b2c30d6522f749abdd018b08c92ef
Asthapuram Chanikya <asthapuramchanikya@gmail.com>
2025-03-08 10:32:07 +0530

    Add files via upload

---
+++ 3 Strings/Exercise.js
@@ -0,0 +1,29 @@
+// 3a Excersise
+'My Name is'
+// 3b Excersise
+'Asthapuram Chanikya'
+// 3c Excersise
+'My Name is Asthapuram Chanikya'
+// 3d Excersise
+'Total cost:$'+(5+3)
+// 3e Excersise
+`Total cost:$ ${5+3}`
+// 3f Excersise
+alert(`Total cost:$ ${5+3}`)
+// 3g Excersise
+'Total cost:$'+(599+295)/100
+// 3h Excersise
+`Total cost:$ ${(599+295)/100}`
+// 3i Excersise
+alert(`Total cost:$ ${(599+295)/100}`)
+// 3j Excersise
+alert(`Total cost:$ ${(599+295)/100}
+Thankyou ,come again`)
+// 3k Excersise
+`Items (${1*2+1*2}):                 $${((2095*2)+(799*2))/100}`
+// 3l Excersise
+`Shipping & handling:                $${(499*2)/100}`
+// 3m Excersise
+`Total before tax:                   $${((2095*2)+(799*2))/100 +( 499*2)/100 }`
+// 3n Excersise
+`Estimated tax(10%):                 $${Math.round((2095*2+799*2+499*2)*0.1)/100}`
