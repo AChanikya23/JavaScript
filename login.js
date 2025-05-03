797ed35f05f5aab5dcf095d1bf9cce9b21c3683e
“AsthapuramChanikya” <asthapuramchanikya@gmil.com>
2025-05-03 19:22:52 +0530

    JT

---
+++ login.js
@@ -0,0 +1,8 @@
+function authenticate(username, password) {
+  if (!username || !password) return false;
+  return username === "admin" && password === "1234";
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = { authenticate };
+}
