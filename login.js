d95033663e3f4d3400e8320139f77aa3a0c2050e
Asthapuram Chanikya <asthapuramchanikya@gmail.com>
2025-05-03 19:43:02 +0530

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-function authenticate(username, password) {
-  if (!username || !password) return false;
-  return username === "admin" && password === "1234";
-}
-
-if (typeof module !== 'undefined') {
-  module.exports = { authenticate };
-}
